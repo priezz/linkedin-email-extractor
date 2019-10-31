ed4beab6515eb53ddb3ad233636c18ec3c1c124f
Alexandr Priezzhev <a@priezz.net>
2019-10-31 17:47:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# linkedin-email-extractor
+Works with redesigned LinkedIn only.
+
+Thanks to [FutoRicky](https://github.com/FutoRicky/linkedin-email-extractor), [anhuin69](https://github.com/anhuin69/linkedin-email-extractor) and [reard96](https://github.com/reard96/linkedin-email-extractor
+) for the initial code. I have completely rewrote it thought.
+
+_Note: After cycling through ~150-200 contacts, it appears that the email addresses begin to populate with your most recent contact's email...I believe that this is LinkedIn trying to prevent bots. Re-starting the script when this happens takes care of the problem. Of course, this is for eductational use only!_
+
+### A node web scraper to extract your linkedin connection emails
+*Currently not being maintained, but open to Pull Requests.*
+
+## Important Note
+Scraping data off of LinkedIn is against their [User Agreement](https://www.linkedin.com/legal/user-agreement). This is purely intended for educational purposes.
+
+If you would like to know the process on making this script, you can read about it [here](https://dev.to/futoricky/how-i-made-a-web-scraper-script-because-linkedin-27fc)
+
+## Why?
+LinkedIn allows you to export all of your connections' info into a csv, except for their emails.
+Additionally their API stopped allowing the extraction of emails around 2013-2014. Why don't we have access to our connections emails through their data export if we both agreed to share that info/data?
+
+## Installation
+- Clone this repo `git clone https://github.com/priezz/linkedin-email-extractor.git` or download
+- Move into the repo directory `cd linkedin-email-extractor`
+- Install dependencies `yarn`
+
+## How to Use
+- You will need the `Connections.csv` file that LinkedIn provides with the data export.
+  - [Instructions on how to export connections from LinkedIn](https://www.linkedin.com/help/linkedin/answer/66844/exporting-connections-from-linkedin?lang=en)
+- Add the `Connections.csv` file into the `linkedin-email-extractor` directory
+- Run `yarn start`
+- Enter LinkedIn Credentials when prompted
+- Wait for email extraction process to finish
+- `Connections.csv` file would be updated on every successful fetch
+
+## LinkedIn UI Versions
+LinkedIn recently updated their UI and it affected the scraper logic. Some people already have the updated UI but some don't, so when you start the script you will be prompted to choose what version do you have. To know what version you have, go into the `network` section and look at the left side panel.
+
+If your panel looks like this, then you are still on the `old` version:
+
+[![Screen-Shot-2019-01-30-at-4-12-12-PM.png](https://i.postimg.cc/L8N31bfb/Screen-Shot-2019-01-30-at-4-12-12-PM.png)](https://postimg.cc/3k0GM9tX)
+
+If your panel looks like this, then you are one the `new` version:
+
+[![Screen-Shot-2019-01-30-at-4-14-34-PM.png](https://i.postimg.cc/rpsCCNNF/Screen-Shot-2019-01-30-at-4-14-34-PM.png)](https://postimg.cc/XZMCnFPT)
+
+## Issues extracting? Read This
+
+If linkedin updates their page and changes the class of an element used in the script it will stop working. You can check out the source code and verify if any class has changed on linkedin and update the script to make it work again. I can't be constantly checking linkedin to see if they have changed something that breaks the script.
---
+++ index.js
@@ -0,0 +1,184 @@
+const csv = require('fast-csv');
+const prompts = require('prompts');
+const Nightmare = require('nightmare');
+
+
+const CSV_FILE = 'Connections.csv';
+const MAX_RETRIES = 3;
+/// Setup prompt attributes
+const OPTIONS = {
+    userId: {
+        message: 'LinkedIn login',
+        type: 'text',
+    },
+    password: {
+        message: 'LinkedIn password',
+        type: 'password',
+    },
+    delayBetweenFetchesMs: {
+        initial: 2000,
+        message: 'Wait interval between each connection search (in ms)',
+        type: 'number',
+    },
+    maxCountToProcess: {
+        initial: 30,
+        message: 'Maximum number of connections to fetch',
+        type: 'number',
+    },
+};
+const PERSON_FIELDS_MAP = {
+    firstName: 'First Name',
+    lastName: 'Last Name',
+    email: 'Email Address',
+    company: 'Company',
+    position: 'Position',
+    connectedOn: 'Connected On',
+    retries: 'Fetch Retries',
+}
+
+
+async function main() {
+    const people = [];
+    csv.parseFile(CSV_FILE, {headers: true})
+        .on('error', (e) => console.error(e.message))
+        .on('data', (personRecord) => {
+            const person = {}
+            Object.keys(PERSON_FIELDS_MAP).forEach(
+                (key) => person[key] = personRecord[PERSON_FIELDS_MAP[key]],
+            )
+            if(person.firstName && person.lastName) people.push(person)
+        })
+        .on('end', () => startProcessing(people));
+}
+
+async function startProcessing(people) {
+    if(!people.length) {
+        console.log('No connections to fetch.');
+        return;
+    }
+
+    const options = await promptOptions();
+
+    const scrapper = new Nightmare({
+        show: false,
+        waitTimeout: 20000,
+    });
+    if(!(await login(scrapper, options.userId, options.password))) return;
+
+    console.log('Logged in, fetching emails...');
+    await fetchEmails(
+        scrapper,
+        people,
+        {
+            connectionsToProcess: Math.min(options.maxCountToProcess, people.length),
+            delayBetweenFetchesMs: options.delayBetweenFetchesMs,
+        },
+    );
+    await scrapper.end();
+    console.log('Fetching finished.');
+    process.exit(0);
+}
+
+
+/// This function starts the process by asking user for LinkedIn credentials, as well config options
+/// - email & password are used to log in to linkedin
+async function promptOptions() {
+    const promptArgs = Object.keys(OPTIONS).map((key) => ({name: key, ...OPTIONS[key]}));
+    return await prompts(promptArgs);
+}
+
+
+/// Log in to LinkedIn
+async function login(scrapper, userId, password) {
+    try {
+        await scrapper
+            .goto('https://www.linkedin.com/uas/login?trk=guest_homepage-basic_nav-header-signin')
+            .insert('#username', userId)
+            .insert('#password', password)
+            .click('.login__form button')
+            .wait('#mynetwork-tab-icon.nav-item__icon');
+        return true;
+    } catch (e) {
+        console.error('An error occured while attempting to login to LinkedIn.');
+    }
+}
+
+
+async function fetchEmails(scrapper, people, {connectionsToProcess, delayBetweenFetchesMs,}) {
+    console.log(`${connectionsToProcess}/${people.length} connections to extract...`);
+    const knownEmails = [];
+    let processedCount = 0;
+    for(const person of people) {
+        if(processedCount >= connectionsToProcess) break;
+        if(person.email) {
+            knownEmails.push(person.email);
+            continue;
+        }
+        if(person.retries >= MAX_RETRIES) continue;
+
+        person.retries = (person.retries || 0) + 1;
+        processedCount++;
+
+        const email = await fetchEmail(
+            scrapper,
+            `${person.firstName} ${person.lastName}`,
+            delayBetweenFetchesMs,
+        );
+        if(knownEmails.includes(email)) {
+            console.error('Stopped due to bot prevention mechanism. Please restart the script.');
+            break;
+        }
+
+        person.email = email;
+        knownEmails.push(email);
+        writePeopleToFile(people);
+    }
+}
+
+
+/// Actual email extraction procedure
+/// Crawler looks for search input box, writes connection name, clicks on first result, copies connection's email
+async function fetchEmail(scrapper, name, delayBetweenFetchesMs) {
+    try {
+        await scrapper
+            .wait('.nav-item--mynetwork')
+            .click('.nav-item--mynetwork a')
+            .wait('.mn-community-summary__link')
+            .click('.mn-community-summary__link')
+            .wait('.mn-connections__search-input')
+            .wait(delayBetweenFetchesMs)
+            .insert('.mn-connections__search-input', name)
+            .wait(2000)
+            .click('.mn-connection-card__link')
+            .wait('[data-control-name=contact_see_more]')
+            .click('[data-control-name=contact_see_more]')
+            .wait('.pv-contact-info');
+
+        /// Here we get the email from the connections linkedin page.
+        const email = await scrapper.evaluate(() => document
+            .querySelector('.pv-contact-info__contact-type.ci-email a.pv-contact-info__contact-link')
+            .getAttribute('href')
+            .replace('mailto:', ''));
+        console.log(`✅  ${name}: ${email}`);
+
+        return email;
+    } catch (e) {
+        console.error(`❌  ${name} unable to extract email`);
+    }
+}
+
+
+function writePeopleToFile(people) {
+    const data = people.map((person) => {
+        const personCsv = {};
+        Object.keys(person).forEach(
+            (key) => personCsv[PERSON_FIELDS_MAP[key]] = person[key],
+        );
+        return personCsv;
+    })
+    csv.writeToPath(CSV_FILE, data, {headers: true}).on('finish', () => {});
+}
+
+
+///* MAIN *///
+main()
